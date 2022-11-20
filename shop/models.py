6eba5f7a884b8b438dbefb106207261235e96fdc
rizwanweb <rizwansoomro@zoho.com>
2022-11-21 01:15:58 +0500

    reinit

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -8,18 +9,23 @@ class Category(models.Model):
     slug = models.SlugField(max_length=250)
 
     class Meta:
-        ordering = ('name',)
+        ordering = ['name']
+        indexes = [ models.Index(fields=['name']),]
         verbose_name = 'category'
         verbose_name_plural = 'catagories'
 
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse("shop:product-list-by-category", args=[self.slug])
+    
+
 # Product Model
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
-    name = models.CharField(max_length=200, db_index=True)
-    slug = models.SlugField(max_length=200, db_index=True)
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
     image = models.ImageField(upload_to='product/%Y/%m/%d', blank=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -29,7 +35,16 @@ class Product(models.Model):
 
     class Meta:
         ordering = ('name',)
+        indexes = [
+            models.Index(fields=['id', 'slug']),
+            models.Index(fields=['name']),
+            models.Index(fields=['-created']),
+        ]
         index_together = (('id', 'slug',))
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("shop:product-detail", args=[self.slug])
+    
--- shop/views.py
+++ shop/views.py
@@ -11,4 +11,11 @@ def product_list(request, category_slug=None):
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-    return render(request, 'shop/product/list.html', {'category': category, 'catagories': catagories, 'products': products})
+    return render(request, 'shop/product/list.html', {
+        'category': category,
+        'catagories': catagories,
+        'products': products})
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    return render(request, 'shop/product/detail.xhtml', {'product': product})
