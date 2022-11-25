bfa20b448ddb157a3c94c5ac2b57ff9e9bdadd29
rizwanweb <rizwansoomro@zoho.com>
2022-11-25 22:57:39 +0500

    finished product list

--- shop/models.py
+++ shop/models.py
@@ -46,5 +46,5 @@ class Product(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse("shop:product-detail", args=[self.slug])
+        return reverse("shop:product-detail", args=[self.id, self.slug])
     
--- shop/urls.py
+++ shop/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = 'shop'
+
 urlpatterns = [
     path('', views.product_list, name='product-list'),
     path('<slug:category_slug>/', views.product_list, name='product-list-by-category'),
     path('<int:id>/<slug:slug>/', views.product_detail, name='product-detail'),
 ]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
