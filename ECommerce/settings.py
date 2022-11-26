8f8f23e71be4ca250c8bae882b3b9cf58b68af91
rizwanweb <rizwansoomro@zoho.com>
2022-11-26 06:46:33 +0500

    Cart class completed

--- ECommerce/settings.py
+++ ECommerce/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'shop',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# SESSION SETUP
+
+CART_SESSION_ID = 'cart'
